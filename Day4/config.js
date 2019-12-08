b64c1747737a8497d4440f987632f61b11bc1827
Dan Thul <djthul@gmail.com>
2019-12-08 08:27:47 -0600

    day4 bug

--- Day4/config.js
+++ Day4/config.js
@@ -1,8 +1,7 @@
 var config = {};
 
 config.endpoint = "https://twentyfivedaychallenge.documents.azure.com:443/";
-config.key =
-  "t8TbR4TSJi5OwG0Ye2CKhSyg3t1Yhu6Om8m2v7tvMa1abDBCsyh58ET1Y3ty42UN7aTnGGtTminQztTpz4stJg==";
+config.key = process.env.twentyfivedaycosoms_DOCUMENTDB_key;
 
 config.database = {
   id: "PotluckDatabase"
